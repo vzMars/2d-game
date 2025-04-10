b3844a46e4ea6ee510b0066552102f1ff6672075
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2025-04-10 10:04:35 -0400

    init game

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+
+public class Game extends GameBase {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inGameLoop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
